26cf4181e9c7d29ea2fcf4d96f9d6424908ede32
Drew Bertola <drewbertola@gmail.com>
2024-05-25 21:16:54 +0700

    auto seed db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Task;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // truncate users and tasks
+        DB::table('users')->truncate();
+        DB::table('tasks')->truncate();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // for owner
+        DB::table('users')->insert([
+            'name' => 'Drew Bertola',
+            'email' => 'drewbertola@gmail.com',
+            'password' => config('app.owner_password'),
         ]);
+
+        // for a guest
+        DB::table('users')->insert([
+            'name' => 'Guest User',
+            'email' => 'guest@example.com',
+            'password' => Hash::make('secret_123'),
+        ]);
+
+        // add some random users
+        User::factory()->count(5)->create();
+
+        // add some tasks
+        Task::factory()->count(68)->create();
+
     }
 }
